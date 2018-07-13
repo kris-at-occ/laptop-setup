57006475c6a9dd19444eed532a9d54fe92ff469b
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-13 08:04:37 -0400

    Update ubuntu-16.04-desktop-setup.sh

--- ubuntu-16.04-desktop-setup.sh
+++ ubuntu-16.04-desktop-setup.sh
@@ -6,7 +6,7 @@ export LANG="en_US.UTF-8"
 
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
-DEBIAN_FRONTEND=noninteractive apt-get -y install vim qemu-kvm lvm2 bridge-utils git
+DEBIAN_FRONTEND=noninteractive apt-get install -y vim qemu-kvm lvm2 bridge-utils git
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i --force-depends google-chrome-stable_current_amd64.deb
